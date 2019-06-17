faf143c84c0237b823df638f3d27b5ffeef46912
Antonio Solari <you@example.com>
2019-06-17 19:16:07 +0200

    added function calculateWinner to get the winner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import Board from "./components/Board";
 import Game from "./styledComponents/styledGame";
 import Title from "./styledComponents/styledTitle";
+import { calculateWinner } from "./calculateWinner";
 function App() {
   const size = 9;
   const [squares, setSquares] = useState(Array(size).fill(null));
   const [turnX, setTurnX] = useState(true);
+  const isWin=calculateWinner(squares);
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || isWin) {
       return;
     }
     const newSquares = squares;
