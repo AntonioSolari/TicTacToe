9792e2c9a666fe4ef5b88d02cec48c6f8cdbe133
Antonio Solari <you@example.com>
2019-06-17 18:33:25 +0200

    format on files

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import Board from "./components/Board";
 import Game from "./styledComponents/styledGame";
 import Title from "./styledComponents/styledTitle";
 function App() {
-  const size=9;
+  const size = 9;
   const [squares, setSquares] = useState(Array(size).fill(null));
-  const [turnX,setTurnX] = useState(true)
-  function handleClick(i){
+  const [turnX, setTurnX] = useState(true);
+  function handleClick(i) {
     if (squares[i]) {
       return;
     }
-    const newSquares=squares
-    console.log(newSquares)
-    newSquares[i]= turnX ? "X" : "O";
-    setSquares(newSquares)
-    setTurnX(prevTurn=> !prevTurn)
-    console.log(turnX)
+    const newSquares = squares;
+    console.log(newSquares);
+    newSquares[i] = turnX ? "X" : "O";
+    setSquares(newSquares);
+    setTurnX(prevTurn => !prevTurn);
+    console.log(turnX);
   }
   return (
     <Game>
       <Title>TicTacToe</Title>
-      <Board squares={squares} onClick={i=>handleClick(i)}/>
+      <Board squares={squares} onClick={i => handleClick(i)} />
     </Game>
   );
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,7 @@
 import React from "react";
 import StyledSquare from "../styledComponents/styledSquare";
 function Square(props) {
-  return <StyledSquare onClick={props.onClick}>
-      {props.value}
-  </StyledSquare>;
+  return <StyledSquare onClick={props.onClick}>{props.value}</StyledSquare>;
 }
 
 export default Square;
